913f2e9f7a764d400d04a5ffd2482dd3ba2bf0d9
jmcvw <43386915+jmcvw@users.noreply.github.com>
2022-06-17 17:13:01 +0100

    Adds loads of stuff to main app

--- app/app.R
+++ app/app.R
@@ -1,38 +1,39 @@
+# library(shiny)
+# library(bs4Dash)
+# source('global.R')
+
 ui <- dashboardPage(dark = NULL, title = 'Understanding Uncertainty',
 
-  header = dashboardHeader(disable = TRUE,
+  header = dashboardHeader(#disable = TRUE,
     title = dashboardBrand(
       title = '',
-      href = "https://codeclan.com",
-      image = "codeclan-logo-white.png"
+      href = 'https://codeclan.com/courses/data-courses/',
+      image = 'codeclan-logo-white.png'
     )
   ),
 
-
-  dashboardSidebar(
+  dashboardSidebar(minified = FALSE, width = '200px',
     fluidRow(
-             numericInput('sample_mean', label = 'Average value',
-                          value = mean_init, min = 0, max = 150),
-             numericInput('sample_sd', label = 'Spread of values',
-                          value = 1, min = 1, max = 20),
-             numericInput('n_x', label = 'Number of observations',
-                          value = 2, min = 10, max = 200),
-      column(2,
-             div(width = '200px',
-             img(src = 'freeagent-brand-assets__logo--white/freeagent-logo-white.png'))
-      )
+      purrr::pmap(input_init, numericInput),
+      actionButton('update_btn', 'New sample', ),
+      checkboxInput('show_ci', 'Show error bars?'),
+      radioButtons('alpha', 'Set Confidence level',
+                   choiceNames = paste(c(99, 95, 90), '%'),
+                   choiceValues = 1-c(0.01, 0.05, 0.1), selected = 1-0.05)
     )
   ),
+
   dashboardBody(
     tags$head(tags$link(rel = "stylesheet", type = "text/css", href = 'cc-style-bs4.css')),
 
     fluidRow(
-    column(6,
-           plotOutput('plot1')
-    ),
-    column(6,
-             plotOutput('plot2')
-    )
+      column(6,
+             plotOutput('difference_plot')
+      ),
+      column(6,
+             plotOutput('distr_plot')
+             # distrPlotUI('distr_plot')
+      )
     ),
     valueBoxUI('vb')
   )
@@ -40,96 +41,76 @@ ui <- dashboardPage(dark = NULL, title = 'Understanding Uncertainty',
 
 server <- function(input, output, session) {
 
+  test_data <- reactive({
+    rnorm(input$n_x, input$sample_mean, input$sample_sd)
+  }) %>%
+    bindEvent(input$update_btn, input$n_x, ignoreNULL = FALSE)
+
   x <- reactive({
     req(input$sample_mean, input$sample_sd >= 1, input$n_x >= 2)
 
-    res <- t.test(rnorm(input$n_x, input$sample_mean, input$sample_sd), mu = expected_mean)
+    res <- t.test(test_data(), mu = expected_mean, conf.level = as.numeric(input$alpha))
 
     list(estimate = res$estimate,
-         tval = res$statistic,
-         pval = res$p.val,
-         ci_lwr = res$conf.int[1],
-         ci_upr = res$conf.int[2],
-         qfun = qnorm(res$p.value),
-         cdf = pnorm(res$p.value),
-         dqfun = dnorm(qnorm(res$p.value)))
+         tval     = res$statistic,
+         pval     = res$p.val,
+         ci_lwr   = res$conf.int[1],
+         ci_upr   = res$conf.int[2],
+         qfun     = qnorm(res$p.value),
+         cdf      = pnorm(res$p.value),
+         dqfun    = dnorm(qnorm(res$p.value)))
   })
 
-
-  output$plot1 <- renderPlot({
-    validate(need(input$sample_mean, 'Please enter a value for the mean.'),
-             need(input$sample_sd >= 1, 'Data must have a spread greater than 0.'),
-             need(input$n_x >= 2, 'Please enter a value of 2 or more for the sample size.'))
+  output$difference_plot <- renderPlot({
+    validate(
+      need(input$n_x >= 2, 'There must be at least 2 observations.'),
+      need(input$sample_mean, 'Enter a value for the average.'),
+      need(input$sample_sd >= 1, 'Enter a value greater than 1 for the spread.')
+    )
 
     par(mar = c(2,6,1,1), bg = pal['students1'])
 
     plot(1, ylim = c(0, 150), axes = FALSE, type = 'n',
-         xlab = '', ylab = '', )
+         xlab = '', ylab = '')
     axis(2, seq(0, 150, 50), las = 1, cex.axis = 2)
     abline(h = expected_mean, col = pal['partners1'], lwd = 3)
     text(.7, y = expected_mean, labels = 'Expected', col = pal['partners1'],
          cex = 1.5, pos = 3)
     text(1, y = x()$estimate, labels = 'Observed', col = pal['psd_blue'],
          cex = 1.5, pos = 4, offset = 2)
-    arrows(1, x()$ci_lwr, 1, x()$ci_upr, angle = 90, code = 3, lwd = 3)
+    if (input$show_ci) arrows(1, x()$ci_lwr, 1, x()$ci_upr, angle = 90, code = 3, lwd = 3)
     points(1, x()$estimate, pch = 21, col = pal['psd_blue1'], bg = pal['data1'], lwd = 4, cex = 3)
   })
 
-  output$plot2 <- renderPlot({
-    req(input$sample_mean, input$sample_sd > 0, input$n_x > 1)
+  output$distr_plot <- renderPlot({
+    sig_threshold <- qnorm((1 - as.numeric(input$alpha)) / 2)
 
     par(mar = c(3, 8, 1, 1), mfrow = c(2, 1),
         xpd = TRUE, bg = pal['students1'])
 
-    plot(distr_data$xs, distr_data$dfun,
-         las = 1, type = 'n',
-         xlab = '', ylab = '',
-         axes = F, cex = 4)
-
-    mtext('Probability\ndensity\n', side = 2, line = 3,
-          cex = 1.8, col = pal['psd_blue1'], outer = FALSE)
-
+    plot_base(distr_data$xs, distr_data$dfun, FALSE,
+              0:4/10, 'Probability\ndensity\n')
     abline(v = -4:4, col = 'grey90')
-    axis(2, 0:4/10, las = 1, cex.axis = 2)
-
-    lines(distr_data$xs, distr_data$dfun, lwd = 6, col = pal['psd_blue1'])
-
-    points(0, dnorm(0), pch = '|', cex = 2, col = pal['psd_blue1'])
-
-    points(x()$qfun, x()$dqfun,
-           pch = 21, col = pal['psd_blue1'], bg = pal['data1'],
-           lwd = 4, cex = 3)
-
-    abline(v = qnorm(c(0.025, 0.975)), col = pal['partners1'], lwd = 2)
-
-    segments(x()$qfun, x()$dqfun,
-             x()$qfun, -10, col = pal['data1'], lwd = 2.5)
+    add_p_curve(distr_data, 'd')
+    abline(v = sig_threshold, col = pal['partners1'], lwd = 3)
+    add_prob_lines(x()$qfun, x()$dqfun, -10)
 
     # --------------------------------------------- #
 
-    plot(distr_data$xs, distr_data$pfun, cex.axis = 2,
-         las = 1, type = 'n', bty = 'n',
-         xlab = '', ylab = '')
-    mtext('Cumulative\ndistribution\n', side = 2, line = 3,
-          cex = 1.8, col = pal['psd_blue1'], outer = F)
+    plot_base(distr_data$xs, distr_data$pfun, TRUE,
+              seq(0, 1, .25), 'Cumulative\ndistribution\n')
     segments(-4:4, rep(-.05, 9), -4:4, rep(2, 9), col = 'grey90')
-
-    lines(distr_data$xs, distr_data$pfun, lwd = 6, col = pal['psd_blue1'])
-
-    points(0, 0.5, pch = '|', cex = 2, col = pal['psd_blue1'])
-    points(x()$qfun, x()$pval,
-           pch = 21, col = pal['psd_blue1'], bg = pal['data1'],
-           lwd = 4, cex = 3)
-    segments(x()$qfun, x()$pval,
-             x()$qfun, 10, col = pal['data1'], lwd = 2.5)
-    segments(qnorm(c(0.025, 0.975)), -.1, qnorm(c(0.025, 0.975)), 1.2,
-             col = pal['partners1'], lwd = 2)
-
+    add_p_curve(distr_data, 'p')
+    segments(sig_threshold, -.1, sig_threshold, 1.2,
+             col = pal['partners1'], lwd = 3)
+    add_prob_lines(x()$qfun, x()$pval, 10)
 
   })
+  # distrPlotServer('distr_plot', x, distr_data, isolate(input$alpha), pal)
 
   valueBoxServer('vb', x)
 }
 
+shinyApp(ui, server, onStart = \() source('global.R'))
 
-shinyApp(ui, server, options = list(port = 4466), onStart = \() source('global.R'))
+
--- app/global.R
+++ app/global.R
@@ -1,5 +1,5 @@
-# library(shiny)
-# library(bs4Dash)
+library(shiny)
+library(bs4Dash)
 
 # Values / data -----------------------------------------------------------
 
@@ -14,7 +14,7 @@ pal <- c(students1  = "#38b0e3",
          psd_blue2  = "#0f252f")
 
 expected_mean <- 100
-mean_init <- sample(seq(90, 120, 5), 1)
+mean_init <- 100#sample(seq(90, 120, 5), 1)
 
 res <- t.test(rnorm(100))
 p <- res$p.value
@@ -26,27 +26,165 @@ distr_data <- list(
   xs = xs, dfun = dnorm(xs), pfun = pnorm(xs)
 )
 
+
+# Init --------------------------------------------------------------------
+
+input_init <- list(inputId = c('n_x', 'sample_mean', 'sample_sd'),
+                   label   = c('Number of observations', 'Average value', 'Spread of values'),
+                   value   = c(2, mean_init, 20),
+                   min     = c(10, 0, 10),
+                   max     = c(200, 150, 20))
+
+
+# Carousel ----------------------------------------------------------------
+
+# carousel(indicators = FALSE,
+#          id = "logoCarousel",
+#          carouselItem(
+#            img(src = 'freeagent-brand-assets__logo--white/freeagent-logo-white.png', width = 180)),
+#          carouselItem(
+#            p('Logos of other partners that we deliver this to?')
+#          ),
+#          carouselItem(
+#            img(src = 'codeclan-logo-white.png',
+#                href = 'https://codeclan.com/courses/data-courses/', width = 180))
+# )
+carouselUI <- function() {
+  logo_paths <- dir('www', patt = 'png$')
+# tagList(
+  carousel(indicators = FALSE,
+           id = "logoCarousel",
+           # lapply(c(logo_paths[3:2]), \(.) carouselItem(img(src = ., width = 180)))
+           # purrr::map(c(logo_paths[3:2]), ~carouselItem(img(src = ., width = 180)))
+  )
+  # )
+}
+
 # Modules -----------------------------------------------------------------
 
 
-valueBoxUI <- function(id) {
+# Diff plot ------------------------------------------------------
+
+diffPlotUI <- function(id) {
   ns <- NS(id)
   tagList(
+    plotOutput(ns('diff_plot'))
+  )
+}
+
+diffPlotServer <- function(id, x, pal, input) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      output$diff_plot <- renderPlot({
+        validate(need(input$sample_mean, 'Please enter a value for the mean.'),
+                 need(input$sample_sd >= 1, 'Enter a spread of at leat 1.'),
+                 need(input$n_x >= 2, 'The sample size must be at least 2.'))
+
+        par(mar = c(2,6,1,1), bg = pal['students1'])
+
+        plot(1, ylim = c(0, 150), axes = FALSE, type = 'n',
+             xlab = '', ylab = '')
+        axis(2, seq(0, 150, 50), las = 1, cex.axis = 2)
+        abline(h = expected_mean, col = pal['partners1'], lwd = 3)
+        text(.7, y = expected_mean, labels = 'Expected', col = pal['partners1'],
+             cex = 1.5, pos = 3)
+        text(1, y = x()$estimate, labels = 'Observed', col = pal['psd_blue'],
+             cex = 1.5, pos = 4, offset = 2)
+        arrows(1, x()$ci_lwr, 1, x()$ci_upr, angle = 90, code = 3, lwd = 3)
+        points(1, x()$estimate, pch = 21, col = pal['psd_blue1'], bg = pal['data1'], lwd = 4, cex = 3)
+      })
+    })
+}
+
+# Distribution plot ------------------------------------------------------
+
+distrPlotUI <- function(id) {
+  ns <- NS(id)
+  tagList(
+    radioButtons(ns('alpha'), 'Set Confidence level',
+                 choiceNames = paste(c(99, 95, 90), '%'),
+                 choiceValues = 1-c(0.01, 0.05, 0.1)),
+    plotOutput(ns('distr_plot'))
+  )
+}
+
+distrPlotServer <- function(id, x, distr_data, sig, pal) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      output$distr_plot <- renderPlot({
+        sig_threshold <- qnorm((1 - as.numeric(sig)) / 2)
+
+        par(mar = c(3, 8, 1, 1), mfrow = c(2, 1),
+            xpd = TRUE, bg = pal['students1'])
+
+        plot(distr_data$xs, distr_data$dfun,
+             las = 1, type = 'n',
+             xlab = '', ylab = '',
+             axes = FALSE, cex = 4)
+
+        mtext('Probability\ndensity\n', side = 2, line = 3,
+              cex = 1.8, col = pal['psd_blue1'], outer = FALSE)
+
+        abline(v = -4:4, col = 'grey90')
+        axis(2, 0:4/10, las = 1, cex.axis = 2)
+
+        lines(distr_data$xs, distr_data$dfun, lwd = 6, col = pal['psd_blue1'])
+        points(0, dnorm(0), pch = '|', cex = 2, col = pal['psd_blue1'])
+
+        abline(v = sig_threshold, col = pal['partners1'], lwd = 3)
+
+        points(x()$qfun, x()$dqfun,
+               pch = 21, col = pal['psd_blue1'], bg = pal['data1'],
+               lwd = 4, cex = 3)
+        segments(x()$qfun, x()$dqfun,
+                 x()$qfun, -10, col = pal['data1'], lwd = 2.5)
+
+        # --------------------------------------------- #
+
+        plot(distr_data$xs, distr_data$pfun, cex.axis = 2,
+             las = 1, type = 'n', bty = 'n',
+             xlab = '', ylab = '')
+        mtext('Cumulative\ndistribution\n', side = 2, line = 3,
+              cex = 1.8, col = pal['psd_blue1'], outer = F)
+        segments(-4:4, rep(-.05, 9), -4:4, rep(2, 9), col = 'grey90')
+
+        lines(distr_data$xs, distr_data$pfun, lwd = 6, col = pal['psd_blue1'])
+        points(0, 0.5, pch = '|', cex = 2, col = pal['psd_blue1'])
+
+        segments(sig_threshold, -.1, sig_threshold, 1.2,
+                 col = pal['partners1'], lwd = 3)
+
+        points(x()$qfun, x()$pval,
+               pch = 21, col = pal['psd_blue1'], bg = pal['data1'],
+               lwd = 4, cex = 3)
+        segments(x()$qfun, x()$pval,
+                 x()$qfun, 10, col = pal['data1'], lwd = 2.5)
+
+
+      })
+    }
+  )
+}
+
+
+# Valueboxes --------------------------------------------------------------
+
+valueBoxUI <- function(id) {
+  tagList(
     fluidRow(
-      bs4ValueBoxOutput(ns('pval'), width = 3),
-      bs4ValueBoxOutput(ns('ci'), width = 3),
-      bs4ValueBoxOutput(ns('dqfun'), width = 3),
-      bs4ValueBoxOutput(ns('cdf'), width = 3)
+      purrr::map(NS(id, c('pval', 'ci', 'dqfun', 'cdf')), width = 3, valueBoxOutput)
     )
   )
 }
 
 
-valueBoxServer <- function(id, x, sample_mean, sample_sd, n_x) {
+valueBoxServer <- function(id, x) {
   moduleServer(id,
                function(input, output, session) {
-                 output$pval <- renderbs4ValueBox({
-                   bs4ValueBox(
+                 output$pval <- renderValueBox({
+                   valueBox(
                      value = if (x()$pval < 0.001) paste('< 0.001') else round(x()$pval, 3),
                      subtitle = '',
                      icon = icon('hat-wizard'),
@@ -55,7 +193,7 @@ valueBoxServer <- function(id, x, sample_mean, sample_sd, n_x) {
                    )
                  })
 
-                 output$ci <- renderbs4ValueBox({
+                 output$ci <- renderValueBox({
                    footer_colour <- if (x()$ci_lwr > expected_mean) {
                      'success'
                    } else if (x()$ci_upr < expected_mean) {
@@ -64,7 +202,7 @@ valueBoxServer <- function(id, x, sample_mean, sample_sd, n_x) {
                      'warning'
                    }
 
-                   bs4ValueBox(
+                   valueBox(
                      subtitle = '',
                      value = paste(round(c(x()$ci_lwr, x()$ci_upr), 1), collapse = ' - '),
                      icon = icon('arrows-alt-v'),
@@ -73,23 +211,66 @@ valueBoxServer <- function(id, x, sample_mean, sample_sd, n_x) {
                    )
                  })
 
-                 output$dqfun <- renderbs4ValueBox({
-                   bs4ValueBox(
+                 output$dqfun <- renderValueBox({
+                   valueBox(
                      value = round(x()$dqfun, 3),
                      subtitle = "",
                      icon = icon('info'),
-                     footer = a('Density function ', href = 'stats-definitions.html', target = 'blank')
+                     footer = a('Probability density ', href = 'stats-definitions.html', target = 'blank')
                    )
                  })
 
-                 output$cdf <- renderbs4ValueBox({
-                   bs4ValueBox(
+                 output$cdf <- renderValueBox({
+                   valueBox(
                      value = round(x()$cdf, 3),
                      subtitle = "",
                      icon = icon('info'),
-                     footer = a('Distribution function ', href = 'stats-definitions.html', target = 'blank')
+                     footer = a('Cumulative distribution ', href = 'stats-definitions.html', target = 'blank')
                    )
                  })
                }
   )
+}
+
+
+plot_base <- function(xs, ys, xax = FALSE, yax, ylab) {
+  plot(xs, ys, cex.axis = 2, axes = FALSE,
+       las = 1, type = 'n', bty = 'n',
+       xlab = '', ylab = '')
+
+  if (xax) {
+    xlabs <- seq(min(xs), max(xs), 1)
+    axis(1, xlabs, cex.axis = 2)
+  }
+
+  axis(2, yax, las = 1, cex.axis = 2)
+
+  mtext(ylab, side = 2, line = 3,
+        cex = 1.8, col = pal['psd_blue1'], outer = FALSE)
+
+}
+
+
+add_p_curve <- function(d, type = c('d', 'p')) {
+  type <- match.arg(type)
+
+  if (type == 'd') {
+    y <- dnorm(0)
+    ys <- d$dfun
+  } else {
+    y <- 0.5
+    ys <- d$pfun
+  }
+  lines(d$xs, ys, lwd = 6, col = pal['psd_blue1'])
+  points(0, y, pch = '|', cex = 2, col = pal['psd_blue1'])
+}
+
+add_prob_lines <- function(x, y, y2) {
+  if (x > -4) {
+    points(x, y,
+           pch = 21, col = pal['psd_blue1'], bg = pal['data1'],
+           lwd = 4, cex = 3)
+    segments(x, y,
+             x, y2, col = pal['data1'], lwd = 2.5)
+  }
 }
